fbbb1aaca33e6c6d2c397908a839594ae1d4d52c
Aleksei Faians <af.git@bsrealm.net>
2026-08-21 17:03:15 +0800

    Misc

--- settings.py
+++ settings.py
@@ -3,15 +3,16 @@ CODE_OFFSET_END = 3
 CODE_OFFSET_START = 1
 EOF_MARKER = b"\x00\x00\x00\x00"
 EOF_MARKER_SIZE = 4
-# Exclude non-product placeholder codes:
-# 0x00FE: No_product_id_allocated
-# 0x00FF: 16-bit_product_id
-# 0x0100: Reserved
-# 0x0104: None
-EXCLUDE_CODES = {0x00FE, 0x00FF, 0x0100, 0x0104}
 HEADER_SIZE = 2
 MAX_RECORD_SIZE = 255
 RECORD_HEADER_SIZE = 4
 MAX_NAME_LEN = MAX_RECORD_SIZE - RECORD_HEADER_SIZE
 MIN_RECORD_BYTES = 4
 MIN_ROW_ELEMENTS = 2
+
+# Exclude non-product placeholder codes:
+# 0x00FE: No_product_id_allocated
+# 0x00FF: 16-bit_product_id
+# 0x0100: Reserved
+# 0x0104: None
+EXCLUDE_CODES = {0x00FE, 0x00FF, 0x0100, 0x0104}
